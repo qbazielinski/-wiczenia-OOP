af3f10b4f0307ab535e1e079ee974ce2ed74ecb5
Jakub Zieliński <you@example.com>
2019-10-12 15:55:04 +0200

    add 0 and 1 ex

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("Hello world!")
+
+var student1 = {
+    name: "Jan",
+    adress: "Gdańsk",
+    student_id_number: "111",
+    profile: "biologia",
+    displayFullInfo: function() {console.log(student1)}
+    
+}
+student1.displayFullInfo();
+
+var student2 = {
+    name: "Piotr",
+    adress: "Gdynia",
+    student_id_number: "222",
+    profile: "chemia",
+    displayFullInfo: function() {console.log(student2)}
+    
+}
+student2.displayFullInfo();
